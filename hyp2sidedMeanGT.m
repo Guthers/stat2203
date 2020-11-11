5039f6d53295c1dfed73cc77deac1a82a4167633
s4434287 <s4434287@moss.labs.eait.uq.edu.au>
2020-11-11 16:43:34 +1000

    More fixes

--- hyp2sidedMeanGT.m
+++ hyp2sidedMeanGT.m
@@ -1,9 +1,9 @@
-function result = hyp2sided(n1, mu1, sd1, n2, mu2, sd2, hyp)
+function result = hyp2sidedMeanGT(n1, mu1, sd1, n2, mu2, sd2, hyp)
     if ~exist('hyp','var')
         hyp = 0;
     end
     poolsqrd  =  (((n1 - 1) * sd1^2) + (n2 - 1) * sd2^2)/ (n1 + n2 - 2)
     tstat =  ((mu1 - mu2) - hyp) / (sqrt(poolsqrd)*sqrt((1/n1) + (1/n2)))
-    pval = tcdf(tstat,n1+n2,'upper')
+    pval = tcdf(tstat,n1+n2-2,'upper')
     result = estrength(pval);
 end
